a6c02c62a8a3dd15be60a11c732a16fbcee8de24
Desmond Paré <desmondpare@gmail.com>
2021-06-10 21:20:41 -0700

    Added search bar to the site Created JSON table for selective suggestion for search bar

--- JT-react-site/src/App.js
+++ JT-react-site/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Stock from './Stock' ; 
-
+import JSONDATA from './MOCK_DATA.json'
 
 function App() {
   return (
     <div className="App">
       <Stock></Stock>
 
+    
+      
+      <input type = "text" placeholder="Search..." />
+
+
+      {JSONDATA.map((val, key)=> {
+        return <div> {val.Name}</div>
+      })}
     </div>
+
+
   );
 }
+
+
 
 export default App;

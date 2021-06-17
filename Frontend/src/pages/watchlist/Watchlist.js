2f4b4e6999a4ce7f4b56bd19da9240ec1f70f6d2
Desmond Paré <desmondpare@gmail.com>
2021-06-17 14:02:08 -0700

    Updated code for SA

--- Frontend/src/pages/watchlist/Watchlist.js
+++ Frontend/src/pages/watchlist/Watchlist.js
@@ -1,20 +1,12 @@
 import './Watchlist.css';
 import { Link } from "react-router-dom";
 
-function Watchlist() {
-  return (
-    <div className="watchlist-view">
-      <div className="container">
-        <div className="row justify-content-md-center">
-          <div className="col-8">
-            <p>this is watchlist view</p>
-            <Link className="btn btn-primary mt-2" to="/details">Go Detailed View</Link>
-          </div>
-        </div>
-      </div>
 
-    </div>
-  );
-}
+class Watchlist extends React.Component {
+    render() {
+        
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
 
-export default Watchlist;
+export default Details;
